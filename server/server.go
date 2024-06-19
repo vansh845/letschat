d3866f4afda0f1bee28cd90609de47459ee6c713
vansh845 <koulvansh845@gmail.com>
2024-06-20 00:59:05 +0530

    refactored middleware

--- server/server.go
+++ server/server.go
@@ -1,20 +1,27 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/vansh845/letschat/server/ws"
 )
 
 func Start(port string) {
+
 	e := echo.New()
-	e.Use(middleware.Logger())
 	e.Static("/static", "ui/dist")
 	e.Static("/assets", "ui/dist/assets")
 
-	e.GET("/chat", ws.WebsocketHandler)
+	logger := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fmt.Println(c.Request().RemoteAddr)
+			return next(c)
+		}
+	}
+
+	e.GET("/chat", ws.WebsocketHandler, logger)
 	e.GET("/group/:groupid", ws.GroupChatHandler)
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "PONG")
