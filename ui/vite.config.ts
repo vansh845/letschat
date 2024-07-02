0c512530b782d0c146544065aa225f32653cd498
vansh845 <koulvansh845@gmail.com>
2024-07-02 18:03:06 +0530

    web service changed to backend

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build:{
+    rollupOptions:{
+      input:{
+        main: path.resolve(__dirname,'index.html')
+      }
+    }
+  },
   server:{
     proxy:{
       "/api" : "http://localhost:3000"
